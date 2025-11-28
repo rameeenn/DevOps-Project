7d96214cc2ab9ead4aa3fec6f4e2fd44cd2a59c9
Maham Farooqi <156342699+Maham-Farooqi@users.noreply.github.com>
2025-11-28 14:32:29 +0500

    Update mysqlserver.js

--- backend/mysqlserver.js
+++ backend/mysqlserver.js
@@ -16,6 +16,7 @@ async function connectWithRetry() {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
+        port: 3306 
       });
       console.log("Connected to MySQL!");
       connected = true;
